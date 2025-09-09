048f0209c9fbc736dc60ea8e2fe737e7ed67575d
ɴᴏᴏʙ - ᴍᴜᴋᴇsʜ <108251605+Noob-mukesh@users.noreply.github.com>
2025-09-09 21:02:26 +0530

    first week

---
+++ week1/leapyear.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+int main(){
+    int year;
+    printf("enter year to check leap or not: ");
+    scanf("%d",&ye);
+    temp=year%100;
+    if (temp==0){
+        if (year%4==0){
+            printf("%d is leap year",year);
+        }
+        {
+        else
+        {
+            printf("%d is ordinary year ",year);
+        }
+            
+        }
+    }
+    {
+        
+    }
+
+
+
+
+}
